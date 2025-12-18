f05bf85686a3cd753a99f0cac0b06cd1678dc828
Corentin FERRY <c.ferry78@laposte.net>
2025-12-18 15:26:49 +0100

    add commentary in main.py

--- main.py
+++ main.py
@@ -2,25 +2,32 @@ def part1 ():
 
     lines = []
 
-    
+    # Permet de récuperer les données dans input.txt lignes par lignes
     with open("input.txt") as file:
         lines = file.readlines()
     
+    # Création de 2 listes 
     firstList = []
     secondList = []
 
+    # Parcours le tableau récuperé depuis input.txt
     for i in range(len(lines)):
+        #Ajoute dans le tableau firstList l'entier de la 1ère valeur de la ligne
         firstList.append(int(lines[i].split(' ')[0]))
+        #Ajoute dans le tableau secondList l'entier de la dernière valeur de la ligne
         secondList.append(int(lines[i].split(' ')[-1]))
 
+    # Tri des 2 listes
     firstList.sort()
     secondList.sort()
     
     total = 0 
 
+    # Parcours mes listes pour calculer la différence et l'ajouter au total
     for i in range(len(firstList)):
         total += abs(firstList[i] - secondList[i])
     
+    # Affiche le total
     print(total)
 
 part1()
